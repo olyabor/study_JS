ab6ff15399b2f7049c363651cc1f849bfab576a5
Olga Borisenko <olyabor800@gmail.com>
2020-08-07 13:27:52 +0300

    Урок 2

--- script/script.js
+++ script/script.js
@@ -1,10 +1,21 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 600;
+let income = 'фриланс';
+let addExpenses = 'Интернет, такси, коммуналка';
+let deposit = true;
+let mission = 1000;
+let period = 3;
 
-alert('Hello');
+// тип данных значений переменных money, income, deposit;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+// длина строки addExpenses
+console.log(addExpenses.length);
 
-console.log('Some text');
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+// Привести строку addExpenses к нижнему регистру и разбить строку на массив
+console.log(addExpenses.toLowerCase().split(', '));
+// дневной бюджет
+let budgetDay = money/30;
+console.log(budgetDay);
